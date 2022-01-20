b5007abda2afb31f4bb0c243a3a46bad2ca245f3
day253 <9634619+day253@users.noreply.github.com>
2022-01-20 21:59:04 +0800

    fix goproxy

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,15 @@
 HOMEDIR := $(shell pwd)
-OUTDIR  := $(HOMEDIR)/output
-GO      := go
-GOPATH  := $(shell $(GO) env GOPATH)
+OUTDIR := $(HOMEDIR)/output
+GO := go
+GOPATH := $(shell $(GO) env GOPATH)
+
+GOBIN := $(GOPATH)/bin
+
+GO_MAJOR_VERSION = $(shell $(GO) version | cut -c 14- | cut -d' ' -f1 | cut -d'.' -f1)
+GO_MINOR_VERSION = $(shell $(GO) version | cut -c 14- | cut -d' ' -f1 | cut -d'.' -f2)
+
+MINIMUM_SUPPORTED_GO_MAJOR_VERSION = 1
+MINIMUM_SUPPORTED_GO_MINOR_VERSION = 16
 
 all: prepare compile test package
 
@@ -11,11 +19,18 @@ prepare-dep:
 	git config --global http.sslVerify false
 	git config --global url.git@code.aliyun.com:.insteadOf https://code.aliyun.com/
 	git submodule update --init --recursive
+	@if [ $(GO_MAJOR_VERSION) -gt $(MINIMUM_SUPPORTED_GO_MAJOR_VERSION) ]; then \
+		$(GO) install honnef.co/go/tools/cmd/staticcheck@latest; \
+	elif [ $(GO_MINOR_VERSION) -ge $(MINIMUM_SUPPORTED_GO_MINOR_VERSION) ]; then \
+		$(GO) install honnef.co/go/tools/cmd/staticcheck@latest; \
+	else \
+		$(GO) get -u honnef.co/go/tools/cmd/staticcheck; \
+	fi
 
 set-env:
 	$(GO) env -w GO111MODULE=on
 	$(GO) env -w CGO_ENABLED=1
-	$(GO) env -w GOPROXY=https://mirrors.aliyun.com/goproxy/
+	$(GO) env -w GOPROXY=https://goproxy.io,direct
 	$(GO) env -w GONOSUMDB=\*
 	$(GO) env -w GOPRIVATE=code.aliyun.com
 
@@ -29,6 +44,7 @@ test: test-case
 
 test-case: set-env
 	$(GO) test -v ./...
+	$(GOBIN)/staticcheck ./...
 
 package: package-bin
 
