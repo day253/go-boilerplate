bf1f182e8419d859e5d476c4cc595dd594681059
day253 <9634619+day253@users.noreply.github.com>
2022-01-22 00:37:56 +0800

    add check tool

--- Makefile.mk
+++ Makefile.mk
@@ -11,21 +11,27 @@ GO_MINOR_VERSION = $(shell $(GO) version | cut -c 14- | cut -d' ' -f1 | cut -d'.
 MINIMUM_SUPPORTED_GO_MAJOR_VERSION = 1
 MINIMUM_SUPPORTED_GO_MINOR_VERSION = 16
 
-all: prepare compile test package
+all: prepare compile test check package
 
-prepare: prepare-dep
+prepare: prepare-dep go-tools
 
 prepare-dep:
 	git config --global http.sslVerify false
 	git config --global url.git@code.aliyun.com:.insteadOf https://code.aliyun.com/
 	git submodule update --init --recursive
-	@if [ $(GO_MAJOR_VERSION) -gt $(MINIMUM_SUPPORTED_GO_MAJOR_VERSION) ]; then \
-		$(GO) install honnef.co/go/tools/cmd/staticcheck@latest; \
-	elif [ $(GO_MINOR_VERSION) -ge $(MINIMUM_SUPPORTED_GO_MINOR_VERSION) ]; then \
-		$(GO) install honnef.co/go/tools/cmd/staticcheck@latest; \
-	else \
-		$(GO) get -u honnef.co/go/tools/cmd/staticcheck; \
-	fi
+
+go-tools:
+	@for repo in \
+		"honnef.co/go/tools/cmd/staticcheck" \
+		"golang.org/x/tools/cmd/goimports" \
+		"github.com/go-critic/go-critic/cmd/gocritic" \
+	; do \
+		if [ $(GO_MINOR_VERSION) -ge $(MINIMUM_SUPPORTED_GO_MINOR_VERSION) ]; then \
+			$(GO) install -v $$repo@latest; \
+		else \
+			$(GO) get -u -v $$repo; \
+		fi \
+	done
 
 set-env:
 	$(GO) env -w GO111MODULE=on
@@ -34,17 +40,27 @@ set-env:
 	$(GO) env -w GONOSUMDB=\*
 	$(GO) env -w GOPRIVATE=code.aliyun.com
 
-compile:build
+compile: build
 
 build: set-env
-	$(GO) mod tidy
-	$(GO) build
+	$(GO) mod tidy -v
+	#$(GO) mod vendor
+	#$(GO) build
 
 test: test-case
 
 test-case: set-env
-	$(GO) test -v ./...
+	#$(GO) test -v ./...
+	$(GO) test -tags=unit -timeout 30s -short -v ./...
+
+check: staticcheck gocritic
+
+staticcheck:
 	$(GOBIN)/staticcheck ./...
+	#$(go) vet ./...
+
+gocritic:
+	$(GOBIN)/gocritic check ./...
 
 package: package-bin
 
@@ -54,4 +70,4 @@ package-bin:
 clean:
 	rm -rf $(OUTDIR)
 
-.PHONY: all prepare compile test package clean build
+.PHONY: all prepare compile test check package clean build
