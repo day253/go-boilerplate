d5b0d3a0c813c84b325c8e82282d7cc215ab94c2
day253 <9634619+day253@users.noreply.github.com>
2024-06-13 23:30:30 +0800

    add port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net"
 
 	api "github.com/cloudwego/kitex-examples/hello/kitex_gen/api/hello"
+	"github.com/cloudwego/kitex/server"
+)
+
+var (
+	port = flag.Int("port", 8888, "port")
 )
 
 func main() {
-	svr := api.NewServer(new(HelloImpl))
+	addr, _ := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", *port))
+	svr := api.NewServer(new(HelloImpl), server.WithServiceAddr(addr))
 
 	err := svr.Run()
 	if err != nil {
