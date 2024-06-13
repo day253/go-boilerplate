8965c4dcec31ec1e4bdddf53a91a439523c30d95
day253 <9634619+day253@users.noreply.github.com>
2024-06-13 23:45:26 +0800

    merge all

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 
-	api "github.com/cloudwego/kitex-examples/hello/kitex_gen/api/hello"
+	"github.com/cloudwego/kitex-examples/hello/kitex_gen/api"
+	"github.com/cloudwego/kitex-examples/hello/kitex_gen/api/hello"
 	"github.com/cloudwego/kitex/server"
 )
 
@@ -14,10 +16,21 @@ var (
 	port = flag.Int("port", 8888, "port")
 )
 
+// HelloImpl implements the last service interface defined in the IDL.
+type HelloImpl struct{}
+
+// Echo implements the HelloImpl interface.
+func (s *HelloImpl) Echo(ctx context.Context, req *api.Request) (resp *api.Response, err error) {
+	// TODO: Your code here...
+	resp = &api.Response{Message: req.Message}
+	log.Println(resp)
+	return
+}
+
 func main() {
 	flag.Parse()
 	addr, _ := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", *port))
-	svr := api.NewServer(new(HelloImpl), server.WithServiceAddr(addr))
+	svr := hello.NewServer(new(HelloImpl), server.WithServiceAddr(addr))
 
 	err := svr.Run()
 	if err != nil {
